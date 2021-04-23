8ede40496d3575b4235c6e67a3b911bd7f946025
sajaljain24 <jain.sj24@gmail.com>
2021-04-24 01:19:27 +0530

    redux app commit

---
+++ src/components/Input.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import './Input.css'
+import { useDispatch } from 'react-redux'
+import { saveTodo } from '../features/todoSlice'
+
+const Input = () => {
+    const [input, setInput] = useState('');
+    const dispatch = useDispatch()
+    const addTodo = () => {
+        console.log(`Adding ${input}`);
+
+        dispatch(saveTodo({
+            item: input,
+            done: false,
+            id: Date.now()
+        }))
+    }
+    return (
+        <div className="input">
+            <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+            <button type="submit" onClick={addTodo}>Add!</button>
+        </div>
+    )
+}
+
+export default Input
